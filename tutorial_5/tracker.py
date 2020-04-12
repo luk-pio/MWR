5fe889a6aa161727d91f398853ec0d654c146a96
Lukasz Piotrak <luke.piotrak@gmail.com>
2020-04-12 12:11:27 +0200

    Finished tracker

--- tutorial_5/tracker.py
+++ tutorial_5/tracker.py
@@ -1,95 +1,105 @@
+import time
+import argparse
 import cv2
 import sys
 
-if __name__ == '__main__':
 
-    # Set up tracker.
-    # Instead of MIL, you can also use
+def main(input, tracker_type, fps_max):
+    trackers = {
+            'boosting': cv2.TrackerBoosting_create,
+            'mil': cv2.TrackerMIL_create,
+            'kcf': cv2.TrackerKCF_create,
+            'tld': cv2.TrackerTLD_create,
+            'medianflow': cv2.TrackerMedianFlow_create,
+            'goturn': cv2.TrackerGOTURN_create,
+            'mosse': cv2.TrackerMOSSE_create,
+            'csrt': cv2.TrackerCSRT_create,
+    }
 
-    tracker_types = ['BOOSTING', 'MIL', 'KCF', 'TLD', 'MEDIANFLOW', 'GOTURN',
-                     'MOSSE', 'CSRT']
-    tracker_type = tracker_types[2]
+    tracker = trackers[tracker_type]()
+    video = cv2.VideoCapture(input)
 
-    if tracker_type == 'BOOSTING':
-        tracker = cv2.TrackerBoosting_create()
-    if tracker_type == 'MIL':
-        tracker = cv2.TrackerMIL_create()
-    if tracker_type == 'KCF':
-        tracker = cv2.TrackerKCF_create()
-    if tracker_type == 'TLD':
-        tracker = cv2.TrackerTLD_create()
-    if tracker_type == 'MEDIANFLOW':
-        tracker = cv2.TrackerMedianFlow_create()
-    if tracker_type == 'GOTURN':
-        tracker = cv2.TrackerGOTURN_create()
-    if tracker_type == 'MOSSE':
-        tracker = cv2.TrackerMOSSE_create()
-    if tracker_type == "CSRT":
-        tracker = cv2.TrackerCSRT_create()
-
-    # Read video
-    video = cv2.VideoCapture("videos/red_ball.mp4")
-
-    # Exit if video not opened.
-    if not video.isOpened():
-        print
-        "Could not open video"
+    if fps_max not in range(1, 120):
+        print(
+            "The max fps parameter must be within the range 1 - 120. "
+            "Aborting...")
         sys.exit()
 
-    # Read first frame.
+    if not video.isOpened():
+        print("Could not open video")
+        sys.exit()
+
     ok, frame = video.read()
     if not ok:
-        print
-        'Cannot read video file'
+        print('Cannot read video file')
         sys.exit()
 
-    # Define an initial bounding box
-    bbox = (287, 23, 86, 320)
+    (H, W) = frame.shape[:2]
 
-    # Uncomment the line below to select a different bounding box
-    bbox = cv2.selectROI(frame, False)
+    bbox = None
 
-    # Initialize tracker with first frame and bounding box
-    ok = tracker.init(frame, bbox)
-
+    prev_time = time.time()
     while True:
-        # Read a new frame
+
+        # Throttle fps
+        time_elapsed = time.time() - prev_time
+        if time_elapsed < 1 / fps_max:
+            continue
+
         ok, frame = video.read()
         if not ok:
             break
 
-        # Start timer
-        timer = cv2.getTickCount()
+        if bbox is not None:
+            (success, box) = tracker.update(frame)
 
-        # Update tracker
-        ok, bbox = tracker.update(frame)
+            if success:
+                (x, y, w, h) = [int(v) for v in box]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        # Calculate Frames per second (FPS)
-        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+        # Define the info we want to show on the video
+        info = [
+                ("Pause: ", "p"),
+                ("Resume: ", "Spc or Enter"),
+                ("Quit: ", "q"),
+                ("Tracker", tracker_type),
+        ]
+        # Display info on the video
+        for (i, (k, v)) in enumerate(info):
+            text = "{}: {}".format(k, v)
+            cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
-        # Draw bounding box
-        if ok:
-            # Tracking success
-            p1 = (int(bbox[0]), int(bbox[1]))
-            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
-        else:
-            # Tracking failure
-            cv2.putText(frame, "Tracking failure detected", (100, 80),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
 
-        # Display tracker type on frame
-        cv2.putText(frame, tracker_type + " Tracker", (100, 20),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
+        if key == ord("p"):
+            bbox = cv2.selectROI("Frame", frame, fromCenter=False,
+                                 showCrosshair=True)
+            tracker = trackers[tracker_type]()
+            tracker.init(frame, bbox)
+        elif key == ord("q"):
+            break
+        prev_time = time.time()
 
-        # Display FPS on frame
-        cv2.putText(frame, "FPS : " + str(int(fps)), (100, 50),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50, 170, 50), 2);
+    cv2.destroyAllWindows()
 
-        # Display result
-        cv2.imshow("Tracking", frame)
 
-        # Exit if ESC pressed
-        k = cv2.waitKey(1) & 0xff
-        if k == ord("q"): break
+def parse_arguments():
+    desc = 'Object tracking software for MWR classes'
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument('-i', '--input', type=str, help='Path to input video')
+    parser.add_argument('-f', '--fps', type=int, help='Max fps', default=30)
+    parser.add_argument('-t', '--tracker', type=str, default='mil',
+                        choices=['boosting', 'mil', 'kcf', 'tld', 'medianflow',
+                                 'goturn', 'mosse', 'csrt'],
+                        help='''OpenCV object tracking algorithm
+                        Can be one of: boosting, mil, kcf, tld, medianflow, 
+                        goturn, mosse, csrt.
+                        ''')
+    return parser.parse_args()
 
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    main(args.input, args.tracker, args.fps)
